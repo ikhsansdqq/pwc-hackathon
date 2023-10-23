0dd149fd941d2e6b293c5963a346a4b0a75d3be3
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2023-10-23 22:34:27 +0700

    updating flask and nextjs

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, Response, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/users/*": {"origins": "*"}})
+
+users = [
+    {
+        "id": 1,
+        "name": "John Doe",
+        "email": "john.doe@example.com"
+    }, {
+        "id": 2,
+        "name": "Jane Doe",
+        "email": "jane.doe@example.com"
+    }, {
+        "id": 3,
+        "name": "Josh Duhamel",
+        "email": "josh@email.com"
+    }
+]
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/users', methods=["GET", "POST"])
+def get_users():
+    return jsonify(users)
+
+
+@app.route('/users/{item_id}', methods=["GET", "POST", "PUT", "DELETE"])
+def get_id(item_id):
+    user = [user for user in users if user["id"] == item_id][0]
+    return jsonify(user)
+
+
+@app.errorhandler(404)
+def not_found():
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
